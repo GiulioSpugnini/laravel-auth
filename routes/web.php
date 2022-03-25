f72d4c3215b892371ded4e88ffb406e4da458e64
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-25 16:56:52 +0100

    created new HomeController for admin and PostController

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
-});
+    return view('guest.home');
+})->name('guest.home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('posts', 'PostController');
+    });
