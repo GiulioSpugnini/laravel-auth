e77a591830ca621e97eef1db75c96c224da664f3
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-25 17:30:12 +0100

    Created App.vue and Header.vue

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('guest.home');
 
 Auth::routes();
 
@@ -27,3 +24,7 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('posts', 'PostController');
     });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
